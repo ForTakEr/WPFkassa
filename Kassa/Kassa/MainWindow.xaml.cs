caa7a73fa09aa3bc727dffbe37f984090072582c
ForTakEr <simotonu.prees@tptlive.ee>
2018-03-22 09:30:33 +0200

    Midagi valmis

--- Kassa/Kassa/MainWindow.xaml.cs
+++ Kassa/Kassa/MainWindow.xaml.cs
@@ -20,9 +20,39 @@ namespace Kassa
     /// </summary>
     public partial class MainWindow : Window
     {
+        public List<Ostukorv> List { get; private set; }
+
         public MainWindow()
         {
             InitializeComponent();
+            List = new List<Ostukorv>();
+        }
+
+
+        public class Ostukorv
+        {
+            public string Name { get; set; }
+            public int Quantity { get; set; }
+            public double Price { get; set; }
+        }
+
+        private void Lisa_Click(object sender, RoutedEventArgs e)
+        {
+            if (Convert.ToDouble(ProductPrice.Text) > 0)
+            {
+                Ostukorv_List.Items.Add(new Ostukorv { Name = ProductName.Text, Price = int.Parse(ProductPrice.Text), Quantity = int.Parse(ProductQuantity.Text) });
+                List.Add(new Ostukorv { Name = ProductName.Text, Price = int.Parse(ProductPrice.Text), Quantity = int.Parse(ProductQuantity.Text) });
+            }
+        }
+
+        private void Eemalda_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Osta_Click(object sender, RoutedEventArgs e)
+        {
+
         }
     }
 }
